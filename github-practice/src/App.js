389400369264b4445e479d7b3889ce7a3f7369a2
Meraj721800 <97161834+Meraj721800@users.noreply.github.com>
2022-02-21 05:07:53 -0500

    KP-new

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Meraj from "./Components/MD MERAJ/Meraj";
+
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Meraj/>
     </div>
   );
 }
---
+++ github-practice/src/Components/MD MERAJ/Meraj.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Meraj = () => {
+    return (
+        <div>
+            <h1> Hello i am meraj</h1>
+        </div>
+    );
+};
+
+export default Meraj;
